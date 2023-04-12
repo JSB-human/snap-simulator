0db227d1dc4cebc259c93a89de679b286e35d12f
JSB-human <tommy63196@gmail.com>
2023-04-12 11:53:44 +0900

    read me

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from 'next/app'
 import { DndProvider } from 'react-dnd'
 import { MultiBackend } from 'react-dnd-multi-backend'
 import { HTML5toTouch } from 'rdndmb-html5-to-touch'
+import Head from 'next/head'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Snap Simulator</title>
+      </Head>
       <DndProvider  backend={MultiBackend} options={HTML5toTouch}>  
         <Component {...pageProps} />
       </DndProvider>
